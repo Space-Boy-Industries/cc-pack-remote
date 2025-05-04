1950602ad8468df1d3355077c604ececb524b336
Eric Sexton <foopis23@gmail.com>
2025-05-04 16:02:42 -0400

    added cctracker

---
+++ cctracker.lua
@@ -0,0 +1,11 @@
+return {
+	name = "cctracker",
+	version = "1.0.0",
+	description = "cctracker is a music tracker program written for the CC: Tweaked mod for Minecraft.",
+	author = "Jamac",
+	base_path = "https://raw.githubusercontent.com/James-Dumas/cctracker/refs/heads/master",
+	-- key is remote path, value is local path
+	file_map = {
+		["cctracker.lua"] = "/bin/cctracker.lua",
+	},
+}
