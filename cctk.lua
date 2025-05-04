8cbc43d49f360fec1d0ccf9e99db4ed392be114d
Eric Sexton <foopis23@gmail.com>
2025-05-04 17:11:25 -0400

    added cctk

---
+++ cctk.lua
@@ -0,0 +1,11 @@
+return {
+	name = "ccctk",
+	version = "1.0.0",
+	description = "A widget based GUI library for ComputerCraft",
+	author = "Jamac",
+	base_path = "https://raw.githubusercontent.com/James-Dumas/cctk/main",
+	-- key is remote path, value is local path
+	file_map = {
+		["/cctk"] = "/lib/cctk",
+	},
+}
